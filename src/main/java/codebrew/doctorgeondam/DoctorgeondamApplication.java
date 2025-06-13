56fe67ef21614f50cb1fd2d0d8c990d95b7a6c84
HeoJun <parkteammini@gmail.com>
2025-06-13 22:35:39 +0900

    feat: Spring Boot 프로젝트 초기 설정 추가

---
+++ src/main/java/codebrew/doctorgeondam/DoctorgeondamApplication.java
@@ -0,0 +1,13 @@
+package codebrew.doctorgeondam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorgeondamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorgeondamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codebrew/doctorgeondam/DoctorgeondamApplicationTests.java
@@ -0,0 +1,13 @@
+package codebrew.doctorgeondam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorgeondamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
